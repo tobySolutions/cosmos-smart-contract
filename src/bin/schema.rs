2d77b802987071361965ac6bce184fb2f763363b
tobySolutions <adedejitobiloba7@gmail.com>
2024-03-12 22:39:36 +0100

    chore: publish to crate

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cosmos_smart_contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_cosmos_smart_contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
